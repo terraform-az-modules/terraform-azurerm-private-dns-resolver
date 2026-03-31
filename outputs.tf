85284f800d164f344767ff19cca5fce1e7c6d86c
Vedant <vedant.kalia@clouddrove.com>
2026-03-31 21:52:23 +0530

    feat: Updated Private DNS Resolver  Module

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,68 @@
 ##-----------------------------------------------------------------------------
 ## Outputs
 ##-----------------------------------------------------------------------------
-output "label_order" {
-  value       = local.label_order
-  description = "Label order."
+output "private_dns_resolver_id" {
+  description = "The ID of the Private DNS Resolver."
+  value       = try(azurerm_private_dns_resolver.main[0].id, null)
+}
+
+output "private_dns_resolver_name" {
+  description = "The name of the Private DNS Resolver."
+  value       = try(azurerm_private_dns_resolver.main[0].name, null)
+}
+
+output "inbound_endpoint_ids" {
+  description = "Map of inbound endpoint keys to their IDs."
+  value = {
+    for k, v in azurerm_private_dns_resolver_inbound_endpoint.main :
+    k => v.id
+  }
+}
+
+output "inbound_endpoint_names" {
+  description = "Map of inbound endpoint keys to their names."
+  value = {
+    for k, v in azurerm_private_dns_resolver_inbound_endpoint.main :
+    k => v.name
+  }
+}
+
+output "inbound_endpoint_ip_addresses" {
+  description = "Map of inbound endpoint keys to their private IP addresses."
+  value = {
+    for k, v in azurerm_private_dns_resolver_inbound_endpoint.main :
+    k => v.ip_configurations[0].private_ip_address
+  }
+}
+
+output "outbound_endpoint_ids" {
+  description = "Map of outbound endpoint keys to their IDs."
+  value = {
+    for k, v in azurerm_private_dns_resolver_outbound_endpoint.main :
+    k => v.id
+  }
+}
+
+output "outbound_endpoint_names" {
+  description = "Map of outbound endpoint keys to their names."
+  value = {
+    for k, v in azurerm_private_dns_resolver_outbound_endpoint.main :
+    k => v.name
+  }
+}
+
+output "forwarding_ruleset_ids" {
+  description = "Map of forwarding ruleset keys to their IDs."
+  value = {
+    for k, v in azurerm_private_dns_resolver_dns_forwarding_ruleset.main :
+    k => v.id
+  }
+}
+
+output "forwarding_ruleset_names" {
+  description = "Map of forwarding ruleset keys to their names."
+  value = {
+    for k, v in azurerm_private_dns_resolver_dns_forwarding_ruleset.main :
+    k => v.name
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,8 +1,122 @@
 ##-----------------------------------------------------------------------------
-## Variables
+# Global
 ##-----------------------------------------------------------------------------
+
+variable "enabled" {
+  type        = bool
+  default     = true
+  description = "Enable or disable creation of all SQL Managed Instance resources."
+}
+
+variable "name" {
+  type        = string
+  description = "Base name for resources."
+}
+
+variable "custom_name" {
+  type        = string
+  default     = null
+  description = "Optional custom name to override the base name in tags."
+}
+
+variable "location" {
+  type        = string
+  default     = null
+  description = "Azure region where resources will be deployed."
+}
+
+variable "environment" {
+  type        = string
+  default     = null
+  description = "Deployment environment (e.g., dev, stage, prod)."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = null
+  description = "Name of the resource group where resources will be deployed."
+}
+
+variable "managedby" {
+  type        = string
+  default     = "terraform"
+  description = "Tag to indicate the tool or team managing the resources."
+}
+
+variable "repository" {
+  type        = string
+  default     = "https://github.com/terraform-az-modules/terraform-azurerm-private-dns-resolver.git"
+  description = "Repository URL or identifier for traceability."
+}
+
+variable "deployment_mode" {
+  type        = string
+  default     = "terraform"
+  description = "Deployment mode identifier (e.g., blue/green, canary)."
+}
+
 variable "label_order" {
-  type        = list(any)
+  type        = list(string)
   default     = ["name", "environment", "location"]
-  description = "Label order, e.g. `name`,`application`,`centralus`."
+  description = "Order of labels to be used in naming/tagging."
+}
+
+variable "extra_tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags to apply to all resources."
+}
+
+variable "resource_position_prefix" {
+  type        = bool
+  default     = false
+  description = "If true, prefixes resource names instead of suffixing."
+}
+
+##-----------------------------------------------------------------------------
+# Inbound Endpoint
+##-----------------------------------------------------------------------------
+
+variable "inbound_endpoint_map" {
+  type = map(object({
+    inbound_endpoint_name        = string
+    private_ip_allocation_method = string
+    private_ip_address           = optional(string)
+    inbound_subnet_id            = string
+  }))
+  default     = {}
+  description = "Map of inbound DNS resolver endpoints to create. Each key is a unique identifier for the endpoint."
+}
+
+##-----------------------------------------------------------------------------
+# Outbound Endpoint
+##-----------------------------------------------------------------------------
+
+variable "outbound_endpoint_map" {
+  type = map(object({
+    outbound_endpoint_name = string
+    outbound_subnet_id     = string
+  }))
+  default     = {}
+  description = "Map of outbound DNS resolver endpoints to create. Each key is a unique identifier for the endpoint."
+}
+
+##-----------------------------------------------------------------------------
+# Forwarding Ruleset 
+##-----------------------------------------------------------------------------
+
+variable "outbound_endpoint_forwarding_rule_sets" {
+  type = list(object({
+    outbound_endpoint_name  = string
+    forwarding_ruleset_name = string
+    outbound_endpoint_id    = string
+  }))
+  default     = []
+  description = "List of DNS forwarding rulesets to associate with outbound endpoints. Each entry requires the outbound endpoint name, ruleset name, and outbound endpoint resource ID."
+}
+
+variable "virtual_network_id" {
+  type        = string
+  default     = null
+  description = "The ID of the Virtual Network where this Private DNS Resolver should be located in."
 }
